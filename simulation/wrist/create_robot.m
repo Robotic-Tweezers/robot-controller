d916c94187e5fe66e16c3e95609b6054c62074c5
Jake Cronin <jcronin97@live.ca>
2021-11-18 23:23:26 -0800

    Added sdf

--- simulation/wrist/create_robot.m
+++ simulation/wrist/create_robot.m
@@ -4,9 +4,13 @@ function [robot] = create_robot()
 %   https://www.mathworks.com/help/robotics/ug/build-a-robot-step-by-step.html
 %   https://www.mathworks.com/help/robotics/ug/build-basic-rigid-body-tree-models.html
 
-robot = importrobot("models\wrist.sdf");
-
+robot = importrobot("models\wrist.sdf", 'DataFormat', 'column');
 showdetails(robot);
+
+% (robot,configuration,jointVel,jointTorq,fext)
+f_ext = [0 0 0 0 0 0; 0 0 0 0 0 0]';
+f = forwardDynamics(robot, [0], [1], [1], f_ext);
+
 show(robot,"Collisions","on","Frames","off");
 
 end
