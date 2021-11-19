0857d6f25ea604f9095df818054fcfaa52ddefab
Jake <jcronin97@live.ca>
2021-11-19 11:56:13 -0800

    Progress on sdf model

--- simulation/wrist/create_robot.m
+++ simulation/wrist/create_robot.m
@@ -1,16 +1,14 @@
 function [robot] = create_robot()
 %CREATE_ROBOT Create a robot object used in controls analysis
 %   Detailed explanation goes here
-%   https://www.mathworks.com/help/robotics/ug/build-a-robot-step-by-step.html
-%   https://www.mathworks.com/help/robotics/ug/build-basic-rigid-body-tree-models.html
 
 robot = importrobot("models\wrist.sdf", 'DataFormat', 'column');
 showdetails(robot);
 
 % (robot,configuration,jointVel,jointTorq,fext)
-f_ext = [0 0 0 0 0 0; 0 0 0 0 0 0]';
-f = forwardDynamics(robot, [0], [1], [1], f_ext);
+f_ext = eye(6, 7);
+f = forwardDynamics(robot, [0; pi / 2; 0], [1; 1; 1], [1; 1; 1], f_ext);
 
-show(robot,"Collisions","on","Frames","off");
+show(robot,[0; pi / 2; 0],"Collisions","on","Frames","on", 'PreservePlot',false);
 
 end
