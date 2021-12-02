8fdf39878c8aa58b29d91dd44c63507c56b46add
Jake <jcronin97@live.ca>
2021-12-02 12:59:38 -0800

    Testing multiple steppers

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,22 @@
 #include <coordinates.hpp>
 #include <TMCStepper.h>
 
-#define TIME_IN_MS(TIME) ((TIME)*configTICK_RATE_HZ / 1000UL)
-#define TIME_IN_US(TIME) ((TIME)*configTICK_RATE_HZ / 1000000UL)
-#define STEPPERS (1)
+#define TIME_IN_MS(TIME) (TIME) * configTICK_RATE_HZ / 1000UL
+#define TIME_IN_US(TIME) (TIME) * configTICK_RATE_HZ / 1000000UL
+#define STEPPERS 3
+#define STALL_VALUE 50      // [0... 255]
+#define TOFF_VALUE 4        // [1... 15]
+#define ENABLE_PIN 2        // Enable pin
+#define THETA1_ADDRESS 0b00 // TMC2209 Driver address according to MS1 and MS2
+#define THETA2_ADDRESS 0b01 // TMC2209 Driver address according to MS1 and MS2
+#define THETA3_ADDRESS 0b10 // TMC2209 Driver address according to MS1 and MS2
+#define ADDRESS(STEPPER)    (int8_t)((STEPPER).ms2()) << 1 | (int8_t)((STEPPER).ms1())
+// Match to your driver
+// SilentStepStick series use 0.11
+// UltiMachine Einsy and Archim2 boards use 0.2
+// Panucatt BSD2660 uses 0.1
+// Watterott TMC5160 uses 0.075
+#define R_SENSE 0.11f
 
 using namespace Eigen;
 using namespace RobotTweezers;
@@ -19,35 +32,49 @@ time_t rtc_time;
 const Matrix6f kp = vectorToDiagnol6((float[]){0, 0, 0, 1.00, 1.00, 1.00});
 const Matrix6f kv = vectorToDiagnol6((float[]){0.1, 0.5, 0.1, 0.1, 0.5, 0.1});
 volatile float desired_position;
-Stepper steppers[STEPPERS] =
-    {
-        Stepper(0, 11, 12, 10, 9, 8, 22, 23),
-        // Stepper(1, 3, 4, 5, 6, 7, 12, 21),           // Not used yet
-        // Stepper(2, 14, 15, 16, 17, 18, 19, 20),    // Not used yet
+TMC2209Stepper steppers[STEPPERS] =
+{
+    TMC2209Stepper(&Serial1, R_SENSE, THETA1_ADDRESS),
+    TMC2209Stepper(&Serial2, R_SENSE, THETA2_ADDRESS),
+    TMC2209Stepper(&Serial3, R_SENSE, THETA3_ADDRESS),
 };
 
-// Cannot use local variables to initialize interrupts (so no loop here)
-static inline void configureStepperInterrupts(void)
+static bool stepperInit(TMC2209Stepper& stepper)
 {
-#define REGISTER_ENCODER_INTERRUPTS(IDX) (                                                           \
-    {                                                                                                \
-        attachInterrupt(                                                                             \
-            digitalPinToInterrupt(steppers[(IDX)].encoder.getPinA()), [](void) -> void { \
-            noInterrupts(); \
-            steppers[(IDX)].encoder.pinInterruptA(); \
-            interrupts(); }, CHANGE); \
-        attachInterrupt(                                                                             \
-            digitalPinToInterrupt(steppers[(IDX)].encoder.getPinB()), [](void) -> void { \
-            noInterrupts(); \
-            steppers[(IDX)].encoder.pinInterruptB(); \
-            interrupts(); }, CHANGE); \
-    })
+    static int i = 0;
+    /// @TODO Figure out what these do
+    stepper.begin();
+    stepper.toff(TOFF_VALUE);
+    // Set velocity
+    stepper.VACTUAL(-5000);
+    /// @TODO Figure out what these do
+    stepper.blank_time(24);
+    stepper.rms_current(400);
+    // Set microstep resolution
+    stepper.microsteps(16);
+    /// @TODO Figure out what these do
+    stepper.TCOOLTHRS(0xFFFFF);
+    stepper.semin(5);
+    stepper.semax(2);
+    stepper.sedn(0b01);
+    stepper.SGTHRS(STALL_VALUE);
 
-    REGISTER_ENCODER_INTERRUPTS(0);
-    // REGISTER_ENCODER_INTERRUPTS(1);
-    // REGISTER_ENCODER_INTERRUPTS(2);
+    Serial.println("Testing connection...");
+    uint8_t result = stepper.test_connection();
+    if (result)
+    {
+        char print_buffer[128];
+        sprintf(print_buffer, "Failed to connect to stepper at address %d, likely cause:", i++);//ADDRESS(stepper));
+        Serial.println(print_buffer);
+        result == 1 ? Serial.println("loose connection") : Serial.println("no power");
+        Serial.println("Fix the problem and reset board.");
+        return false;
+    }
+
+    return true;
 }
 
+/*
 static void readJointState(float theta[], Vector3f &theta_dot, float interval)
 {
     for (uint8_t i = 0; i < (uint8_t)STEPPERS; i++)
@@ -107,96 +134,7 @@ static void controlLoop(void *arg)
         vTaskDelay(TIME_IN_MS(loop_period));
     }
 }
-
-static void pollSerial(void *arg)
-{
-    while (true)
-    {
-        if (Serial.available() > 0)
-        {
-            String message = Serial.readString(128);
-            desired_position = message.toFloat();
-            Serial.clear();
-            Serial.println(desired_position);
-        }
-
-        vTaskDelay(TIME_IN_MS(200UL));
-    }
-}
-
-void swuart_calcCRC(uint8_t *datagram, uint8_t datagramLength)
-{
-    uint8_t *crc = datagram + (datagramLength - 1); // CRC located in last byte of message
-    uint8_t currentByte;
-
-    *crc = 0;
-    // Execute for all bytes of a message
-    for (uint8_t i = 0; i < (datagramLength - 1); i++)
-    {
-        currentByte = datagram[i]; // Retrieve a byte to be sent from Array
-        for (uint8_t j = 0; j < 8; j++)
-        {
-            if ((*crc >> 7) ^ (currentByte & 0x01)) // update CRC based result of XOR operation
-            {
-                *crc = (*crc << 1) ^ 0x07;
-            }
-            else
-            {
-                *crc = (*crc << 1);
-            }
-
-            currentByte = currentByte >> 1;
-        } // for CRC bit
-    }     // for message byte
-}
-
-static void manualStepperUartTesting(void* arg)
-{
-    uint8_t buffer[4] = {0xA0, 0x00, 0x6, 0x00}; // , 0x00, 0x00, 0x00, 0x00};
-    char read_buffer[8];
-    swuart_calcCRC(buffer, 4);
-
-    // driver.begin();                 //  SPI: Init CS pins and possible SW SPI pins
-                                    // UART: Init SW UART (if selected) with default 115200 baudrate
-    // driver.toff(4);                 // Enables driver in software
-    // driver.rms_current(2000);       // Set stepper current to 600mA. The command is the same as command TMC2130.setCurrent(600, 0.11, 0.5);
-    // driver.microsteps(16);
-    // driver.pwm_autoscale(true);     // Needed for stealthChop 
-
-    while (true)
-    {
-        Serial1.write(buffer, 4);
-        Serial1.flush();
-        vTaskDelay(TIME_IN_MS(500));
-        if (Serial1.available() > 0)
-        {
-            Serial1.readBytes(read_buffer, 8);
-            sprintf(read_buffer, "%x%x%x%x%x%x%x%x", 
-                read_buffer[0], read_buffer[1], read_buffer[2], read_buffer[3], 
-                read_buffer[4], read_buffer[5], read_buffer[6], read_buffer[7]);
-            Serial.println(read_buffer);
-        }
-        
-        Serial1.clear();
-
-        vTaskDelay(TIME_IN_MS(500));
-    }
-}
-
-#define STALL_VALUE 50 // [0... 255]
-#define TOFF_VALUE 4   // [1... 15]
-
-#define EN_PIN 6 // Enable pin
-#define SW_RX 2  // SoftwareSerial receive pin
-#define SW_TX 3  // SoftwareSerial transmit pin
-//#define SERIAL_PORT Serial1 // HardwareSerial port
-#define DRIVER_ADDRESS 0b00 // TMC2209 Driver address according to MS1 and MS2
-
-#define R_SENSE 0.11f // Match to your driver
-                      // SilentStepStick series use 0.11
-                      // UltiMachine Einsy and Archim2 boards use 0.2
-                      // Panucatt BSD2660 uses 0.1
-                      // Watterott TMC5160 uses 0.075
+*/
 
 TMC2209Stepper driver(&Serial1, R_SENSE, 0x00);
 
@@ -205,43 +143,6 @@ int32_t speed = 5000;
 static void stepperUartTesting(void *arg)
 {
     uint16_t last_position, position;
-    driver.begin();
-    driver.toff(TOFF_VALUE);
-    driver.VACTUAL(speed);
-    driver.blank_time(24);
-    driver.rms_current(400);
-    driver.microsteps(16);
-    driver.TCOOLTHRS(0xFFFFF);
-    driver.semin(5);
-    driver.semax(2);
-    driver.sedn(0b01);
-    driver.SGTHRS(STALL_VALUE);
-    last_position = driver.MSCNT();
-
-    Serial.print("\nTesting connection...");
-    uint8_t result = driver.test_connection();
-
-    if (result)
-    {
-        Serial.println("failed!");
-        Serial.print("Likely cause: ");
-
-        switch (result)
-        {
-        case 1:
-            Serial.println("loose connection");
-            break;
-        case 2:
-            Serial.println("no power");
-            break;
-        }
-
-        Serial.println("Fix the problem and reset board.");
-
-        // We need this delay or messages above don't get fully printed out
-        delay(100);
-        abort();
-    }
 
     Serial.println("OK");
 
@@ -314,7 +215,7 @@ static void stepperUartTesting(void *arg)
         if ((ms - last_time) > 100 && driver.toff() != 0 && speed != 0)
         {
             position = driver.MSCNT();
-            float delta = ((float)position - (float)last_position);// / ((float)ms - (float)last_time);
+            float delta = ((float)position - (float)last_position); // / ((float)ms - (float)last_time);
             last_position = position;
             last_time = ms;
             Serial.print("Status: ");
@@ -346,15 +247,31 @@ void setup()
 
     Serial.begin(9600);
     Serial1.begin(115200);
+    Serial2.begin(115200);
+    Serial3.begin(115200);
 
+    // Enable steppers
+    pinMode(ENABLE_PIN, OUTPUT);
+    digitalWrite(ENABLE_PIN, LOW);
+
+    //for (TMC2209Stepper& stepper : steppers)
+    //{
+    //    status &= stepperInit(stepper);
+    //}
+    status &= stepperInit(steppers[0]);
+    status &= stepperInit(steppers[1]);
+    if (status != pdPASS)
+    {
+        Serial.println("Steppers did not initialize properly");
+        error_state();
+    }
+
+    // Turn on LED to indicate correct operation
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, HIGH);
 
-    // configureStepperInterrupts();
-
-    // status &= xTaskCreate(pollSerial, NULL, configMINIMAL_STACK_SIZE, NULL, 1, NULL);
     // status &= xTaskCreate(controlLoop, NULL, 10 * configMINIMAL_SECURE_STACK_SIZE, NULL, 2, NULL);
-    status &= xTaskCreate(stepperUartTesting, NULL, 10 * configMINIMAL_SECURE_STACK_SIZE, NULL, 1, NULL);
+    // status &= xTaskCreate(stepperUartTesting, NULL, 10 * configMINIMAL_SECURE_STACK_SIZE, NULL, 1, NULL);
 
     if (status != pdPASS)
     {
