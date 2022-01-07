b502c7d48392b55c75100ab7c00acd683501179e
Jake Cronin <jcronin97@live.ca>
2022-01-06 19:00:29 -0800

    Created test project for stepper development

--- lib/stepper/include/stepper.hpp
+++ lib/stepper/include/stepper.hpp
@@ -3,12 +3,6 @@
 
 #include <Arduino.h>
 #include <stdint.h>
-#include <encoder.hpp>
-
-// Could be higher?
-#define MAX_FREQUENCY                   (5000) // Hz
-#define MAX_VELOCITY(RESOLUTION)        ((float)(2 * PI * MAX_FREQUENCY / (200 * (RESOLUTION))))
-#define FREQUENCY(RESOLUTION, VELOCITY) ((float)(200 * (RESOLUTION) * (VELOCITY) / (2 * PI)))
 
 // Useful links
 // https://github.com/bigtreetech/BIGTREETECH-Stepper-Motor-Driver/blob/master/TMC2209/V1.1/manual/TMC2209-V1.1-manual.pdf
@@ -17,32 +11,11 @@
 
 namespace RobotTweezers
 {
-    enum resolution_e
-    {
-        MICROSTEP8 = 8,
-        MICROSTEP32 = 32,
-        MICROSTEP64 = 64,
-        MICROSTEP16 = 16,
-    };
-
     class Stepper
     {
         private:
 
-        uint8_t index;
-        uint8_t step;
-        uint8_t direction;
-        uint8_t enable;
-        uint8_t microstep1;
-        uint8_t microstep2;
-
-        void setDirection(bool positive);
-
-        void setResolution(resolution_e resolution);
-
         public:
-
-        Encoder encoder;
 
         /****************************************
          * Step/Direction control
@@ -53,44 +26,7 @@ namespace RobotTweezers
          */
         Stepper();
 
-        /**
-         * @brief Construct a new Stepper object, uses step and direction pins only
-         * 
-         * @param step 
-         * @param direction 
-         */
-        Stepper(uint8_t index, uint8_t step, uint8_t direction, uint8_t encoder_a, uint8_t encoder_b);
-
-        /**
-         * @brief Construct a new Stepper object, uses step, direction and enable
-         * 
-         * @param step 
-         * @param direction 
-         */
-        Stepper(uint8_t index, uint8_t step, uint8_t direction, uint8_t enable, uint8_t encoder_a, uint8_t encoder_b);
-
-        /**
-         * @brief Construct a new Stepper object
-         * 
-         * @param step 
-         * @param direction 
-         */
-        Stepper(uint8_t index, uint8_t step, uint8_t direction, uint8_t enable, uint8_t microstep1, uint8_t microstep2, 
-            uint8_t encoder_a, uint8_t encoder_b);
-
-        void configureOutputPins(uint8_t step, uint8_t direction, uint8_t enable, uint8_t microstep1, uint8_t microstep2);
-
-        void configureEncoder(uint8_t encoder_a, uint8_t encoder_b);
-        
-        void enableStepper(void);
-        
-        void disableStepper(void);
-
-        /**
-         * @brief 
-         * 
-         */
-        void setVelocity(float velocity);
+        void testStepperComm(void);
     };
 }
 
--- lib/stepper/src/stepper.cpp
+++ lib/stepper/src/stepper.cpp
@@ -1,109 +1,32 @@
 #include <stepper.hpp>
 
-void RobotTweezers::Stepper::setResolution(resolution_e resolution)
-{
-    digitalWrite(microstep1, (int)resolution == 32 || (int)resolution == 16);
-    digitalWrite(microstep2, (int)resolution == 64 || (int)resolution == 16);
-}
+RobotTweezers::Stepper::Stepper() {}
 
-void RobotTweezers::Stepper::setDirection(bool positive)
+static void swuart_calcCRC(uint8_t *datagram, uint8_t datagramLength)
 {
-    digitalWrite(this->direction, positive);
-}
-
-RobotTweezers::Stepper::Stepper() { }
-
-RobotTweezers::Stepper::Stepper(uint8_t index, uint8_t step, uint8_t direction, uint8_t encoder_a, uint8_t encoder_b)
-: index(index), encoder(index, encoder_a, encoder_b, 100)
-{
-    configureOutputPins(step, direction, 255, 255, 255);
-    digitalWrite(step, LOW);
-    digitalWrite(direction, HIGH);
-}
-
-RobotTweezers::Stepper::Stepper(uint8_t index, uint8_t step, uint8_t direction, uint8_t enable, uint8_t encoder_a, uint8_t encoder_b)
-: index(index), encoder(index, encoder_a, encoder_b, 100)
-{
-    configureOutputPins(step, direction, enable, 255, 255);
-    digitalWrite(step, LOW);
-    digitalWrite(direction, HIGH);
-    digitalWrite(enable, LOW);
-}
-
-RobotTweezers::Stepper::Stepper
-    (uint8_t index, uint8_t step, uint8_t direction, uint8_t enable, uint8_t microstep1, uint8_t microstep2, 
-    uint8_t encoder_a, uint8_t encoder_b) : index(index), encoder(index, encoder_a, encoder_b, 100)
-{
-    configureOutputPins(step, direction, enable, microstep1, microstep2);
-    digitalWrite(step, LOW);
-    digitalWrite(direction, HIGH);
-    digitalWrite(enable, LOW);
-    digitalWrite(microstep1, LOW);
-    digitalWrite(microstep2, LOW);
-}
-
-void RobotTweezers::Stepper::configureOutputPins(uint8_t step, uint8_t direction, uint8_t enable, uint8_t microstep1, uint8_t microstep2)
-{
-    this->step = step;
-    this->direction = direction;
-    this->enable = enable;
-    this->microstep1 = microstep1;
-    this->microstep2 = microstep2;
-    
-    pinMode(step, OUTPUT);
-    pinMode(direction, OUTPUT);
-    pinMode(enable, OUTPUT);
-    pinMode(microstep1, OUTPUT);
-    pinMode(microstep2, OUTPUT);
-}
-
-void RobotTweezers::Stepper::configureEncoder(uint8_t encoder_a, uint8_t encoder_b)
-{
-    encoder.configureInputPins(encoder_a, encoder_b);
-}
-
-void RobotTweezers::Stepper::enableStepper(void)
-{
-    digitalWrite(enable, LOW);
-}
-
-void RobotTweezers::Stepper::disableStepper(void)
-{
-    digitalWrite(enable, HIGH);
-}
-
-void RobotTweezers::Stepper::setVelocity(float velocity)
-{
-    float frequency;
-    float velocity_abs = abs(velocity);
-    // Prioritize high resolution
-    if (velocity_abs < MAX_VELOCITY(MICROSTEP64))
-    {
-        setResolution(MICROSTEP64);
-        frequency = FREQUENCY(MICROSTEP64, velocity_abs);
+    int i, j;
+    uint8_t *crc = datagram + (datagramLength - 1); // CRC located in last byte of message
+    uint8_t currentByte;
+    *crc = 0;
+    for (i = 0; i < (datagramLength - 1); i++)
+    {                              // Execute for all bytes of a message
+        currentByte = datagram[i]; // Retrieve a byte to be sent from Array
+        for (j = 0; j < 8; j++)
+        {
+            if ((*crc >> 7) ^ (currentByte & 0x01)) // update CRC based result of XOR operation
+            {
+                *crc = (*crc << 1) ^ 0x07;
+            }
+            else
+            {
+                *crc = (*crc << 1);
+            }
+            currentByte = currentByte >> 1;
+        } // for CRC bit
     }
-    else if (velocity_abs < MAX_VELOCITY(MICROSTEP32))
-    {
-        setResolution(MICROSTEP32);
-        frequency = FREQUENCY(MICROSTEP32, velocity_abs);
-    }
-    else if (velocity_abs < MAX_VELOCITY(MICROSTEP16))
-    {
-        setResolution(MICROSTEP16);
-        frequency = FREQUENCY(MICROSTEP16, velocity_abs);
-    }
-    else if (velocity_abs < MAX_VELOCITY(MICROSTEP8))
-    {
-        setResolution(MICROSTEP8);
-        frequency = FREQUENCY(MICROSTEP8, velocity_abs);
-    }
-    else
-    {
-        setResolution(MICROSTEP8);
-        frequency = MAX_FREQUENCY;
-    }
+}
 
-    setDirection(velocity > 0);
-    analogWriteFrequency(step, frequency);
-    analogWrite(step, 128);
+void RobotTweezers::Stepper::testStepperComm(void)
+{
+    Serial.println("Hello!");
 }
