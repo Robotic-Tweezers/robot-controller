f6d7638243b059939f761266acab89be5f5a90f4
Jake <jcronin97@live.ca>
2022-04-14 15:08:31 -0700

    Moved simulation files to new repo

--- simulation/wrist/createSphericalWrist.m
+++
@@ -1,9 +0,0 @@
-function robot = createSphericalWrist()
-%CREATE_SPHERICAL_WRIST Create a robot object used in controls analysis
-%   Loads SDF description of the spherical wrist used for the robot
-%   tweezers project.
-robot = importrobot("models/sdf/wrist.sdf", 'DataFormat', 'column');
-robot.Gravity = [0 0 -9.81]';
-show(robot, [0 0 0]', "Collisions", "off", "Frames", "off");
-
-end
